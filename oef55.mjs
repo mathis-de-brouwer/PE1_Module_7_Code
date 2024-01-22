7faf06ba886c53a94b749191a932b802e4852ba9
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2024-01-22 19:27:04 +0100

    uhuh

--- oef55.mjs
+++ oef55.mjs
@@ -1,3 +1,4 @@
+/*
 let recept = {
     timemin: 3,
     vegetables: {
@@ -9,5 +10,18 @@ let recept = {
   }
 
   console.log(recept);
+*/
+function createPerson(name, age) {
+    return {
+        name,
+        age,
+        introduce() {
+            return "Hello, my name is " + this.name + " and I am " + this.age + " years old.";
+        }
+    };
+}
+
+let person = createPerson(`Sinterklaas`, 115);
+console.log(person.introduce());
 
   process.exit();
