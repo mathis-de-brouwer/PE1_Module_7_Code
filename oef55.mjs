0cf1d45b590402d574e30a0e435cd51b0e0f8148
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2024-01-22 12:19:19 +0100

    modands

---
+++ oef55.mjs
@@ -0,0 +1,13 @@
+let recept = {
+    timemin: 3,
+    vegetables: {
+        v1: "carrotte",
+        v2: "patate",
+    }, 
+    meat: ["mouton", "chevre", "vache"],
+    spices: ["salt", "pepper"],
+  }
+
+  console.log(recept);
+
+  process.exit();
